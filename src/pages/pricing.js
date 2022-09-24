749af2b5c883882dd2d489e4cdd8aebc6608c2a9
david <david@paradox.ai>
2022-09-23 23:56:56 -0700

    removed broken links

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -1,15 +1,6 @@
 import * as React from "react";
 import PageLayout from "../layouts/PageLayout";
-import About from "../components/About";
-import Hero from "../components/Hero";
-import Sam from "../components/Sam";
-import Reviews from "../components/Reviews";
-import Photoblock from "../components/Photoblock";
 import Pricing from "../components/Pricing";
-import BookNow from "../components/BookNow";
-import Expect from "../components/Expect";
-import Contact from "../components/Contact";
-import Policies from "../components/Policies";
 
 
 const PricingPage = (props) => {
